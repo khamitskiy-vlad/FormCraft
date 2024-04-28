baa9a8e7460ddc1d7a6f29744b31bfcd58b98062
khamitskiy-vlad <vladislaviktorovic@gmail.com>
2024-04-29 00:10:26 +0300

    add load_path to files

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,27 +1,11 @@
 # frozen_string_literal: true
 
+lib = File.expand_path('../../lib/', __FILE__)
+$:.unshift lib unless $:.include?(lib)
+
 require_relative 'hexlet_code/version'
-require_relative 'hexlet_code/tag'
 
 module HexletCode
   class Error < StandardError; end
+  autoload :Tag, 'hexlet_code/tag.rb'
 end
-
-# HexletCode::Tag.build('br')
-# # <br>
-
-# HexletCode::Tag.build('img', src: 'path/to/image')
-# # <img src="path/to/image">
-
-# HexletCode::Tag.build('input', type: 'submit', value: 'Save')
-# # <input type="submit" value="Save">
-
-# # Для парных тегов тело передается как блок
-# HexletCode::Tag.build('label') { 'Email' }
-# # <label>Email</label>
-
-# HexletCode::Tag.build('label', for: 'email') { 'Email' }
-# # <label for="email">Email</label>
-
-# HexletCode::Tag.build('div')
-# # <div></div>
