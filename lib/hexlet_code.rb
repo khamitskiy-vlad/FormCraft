105a704b6a687dac7dea56ecaa8f8c2ac4c39130
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-04-28 13:07:43 +0300

    use autorun and move logic to another file

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,48 +1,27 @@
 # frozen_string_literal: true
 
 require_relative "hexlet_code/version"
-# autoload(:Tag, "lib/hexlet_code/tag.rb")
 
 module HexletCode
   class Error < StandardError; end
-  
-  class Tag
-    def self.build(name, options = {})
-      paired_tags = %w[html table label form span ul p head div]
-      opening_tag = opening_tag(name)
-      attributes = attributes(options)
-      pair_body = yield if block_given?
-      
-      html_form = [] << tag_with_attributes_joiner(opening_tag, attributes)
-
-      if paired_tags.include?(name)
-        html_form << pair_body << "</#{name}>"
-        html_form.join
-      else
-        html_form.join
-      end
-    end
-
-    def self.tag_with_attributes_joiner(open, attr)
-      array = [] << open
-      if attr.empty?
-        array
-      else
-        array << " #{attr}"
-      end
-      array << ">"
-    end 
-
-    def self.opening_tag(name)
-      string = "<#{name}"
-    end
-    
-    def self.attributes(options)
-      array = []
-      options.each do |key, value|
-        array << "#{key}=\"#{value}\""
-      end
-      array.join(" ")
-    end
-  end
+  autoload(:Tag, './hexlet_code/tag')
 end
+
+puts HexletCode::Tag.build('br')
+# <br>
+
+puts HexletCode::Tag.build('img', src: 'path/to/image')
+# <img src="path/to/image">
+
+puts HexletCode::Tag.build('input', type: 'submit', value: 'Save')
+# <input type="submit" value="Save">
+
+# Для парных тегов тело передается как блок
+puts HexletCode::Tag.build('label') { 'Email' }
+# <label>Email</label>
+
+puts HexletCode::Tag.build('label', for: 'email') { 'Email' }
+# <label for="email">Email</label>
+
+puts HexletCode::Tag.build('div')
+# <div></div>
