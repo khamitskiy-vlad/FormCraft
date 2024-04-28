bce337b6a315174aaca3907b44f734de2f1dcb93
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-04-28 13:15:08 +0300

    remove test requests

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -6,22 +6,3 @@ module HexletCode
   class Error < StandardError; end
   autoload(:Tag, './hexlet_code/tag')
 end
-
-puts HexletCode::Tag.build('br')
-# <br>
-
-puts HexletCode::Tag.build('img', src: 'path/to/image')
-# <img src="path/to/image">
-
-puts HexletCode::Tag.build('input', type: 'submit', value: 'Save')
-# <input type="submit" value="Save">
-
-# Для парных тегов тело передается как блок
-puts HexletCode::Tag.build('label') { 'Email' }
-# <label>Email</label>
-
-puts HexletCode::Tag.build('label', for: 'email') { 'Email' }
-# <label for="email">Email</label>
-
-puts HexletCode::Tag.build('div')
-# <div></div>
