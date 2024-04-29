b6a9191959d9716a75485bb162951b8224096d64
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-04-29 16:41:04 +0300

    add form_for method

---
+++ lib/hexlet_code/form.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module HexletCode
+  def self.form_for(_name, url = {})
+    if url.empty?
+      HexletCode::Tag.build('form', action: '#', method: 'post')
+    else
+      url.map do |_key, value|
+        HexletCode::Tag.build('form', action: value, method: 'post')
+      end.join
+    end
+  end
+end
