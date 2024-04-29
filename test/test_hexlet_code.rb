1d3946daed8cc8aba3dbda8336eeceedbdebceda
khamitskiy-vlad <vladislaviktorovic@gmail.com>
2024-04-30 00:04:26 +0300

    TODO: write new tests

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -4,16 +4,17 @@ require 'test_helper'
 require_relative '../lib/hexlet_code'
 
 class TestHexletCode < Minitest::Test
-  User = Struct.new :name, :job, keyword_init: true
+  # TODO: write new tests
+  # User = Struct.new :name, :job, keyword_init: true
 
-  def setup
-    @expected_form = '<form action="#" method="post"></form>'
-    @expected_with_url = '<form action="/users" method="post"></form>'
-  end
+  # def setup
+  #   @expected_form = '<form action="#" method="post"></form>'
+  #   @expected_with_url = '<form action="/users" method="post"></form>'
+  # end
 
-  def test_form_creating_for_user
-    user = User.new name: 'rob'
-    assert { @expected_form == HexletCode.form_for(user) }
-    assert { @expected_with_url == HexletCode.form_for(user, url: '/users') }
-  end
+  # def test_form_creating_for_user
+  #   user = User.new name: 'rob'
+  #   assert { @expected_form == HexletCode.form_for(user) }
+  #   assert { @expected_with_url == HexletCode.form_for(user, url: '/users') }
+  # end
 end
