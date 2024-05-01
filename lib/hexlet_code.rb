a3a866a3978d0ce801e9133c0b25810c16573cab
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-05-01 14:50:12 +0300

    move form_for in main modele

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,17 +4,14 @@ lib = File.expand_path('../../lib/', __FILE__)
 $:.unshift lib unless $:.include?(lib)
 
 require_relative 'hexlet_code/version'
-require_relative 'hexlet_code/form'
 
 module HexletCode
   class Error < StandardError; end
   autoload :Tag, 'hexlet_code/tag.rb'
   autoload :Generator, 'hexlet_code/generator.rb'
+
+  def self.form_for(struct, attr = {}, &)
+    form = Generator.new(struct, **attr, &)
+    HexletCode::Tag.build('form', **form.attr, method: 'post') { form.data }
+  end
 end
-
-# User = Struct.new(:name, :job, :gender, keyword_init: true)
-# user = User.new name: 'rob', job: 'hexlet', gender: 'm'
-
-# HexletCode.form_for user, url: '/users' do |f|
-#   f.input
-# end
--- lib/hexlet_code/form.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module HexletCode
-  def self.form_for(struct, attributes = {}, &)
-    Generator.new struct, attributes, &
-  end
-end
