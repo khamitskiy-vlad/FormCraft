9ad86944d60747ae33b1875a54aee0edae2fe0b3
khamitskiy-vlad <vladislaviktorovic@gmail.com>
2024-05-04 18:03:03 +0300

    change load path

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 lib = File.expand_path('../lib', __dir__)
-$LOAD_PATH.unshift lib unless $:.include?(lib)
+$LOAD_PATH.unshift lib unless $LOAD_PATH.include?(lib)
 
 require_relative 'hexlet_code/version'
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ require 'bundler/setup'
 Bundler.require
 
 lib = File.expand_path('../lib', __dir__)
-$LOAD_PATH.unshift lib unless $:.include?(lib)
+$LOAD_PATH.unshift lib unless $LOAD_PATH.include?(lib)
 
 require 'hexlet_code'
 
