90405c22c546d9b69e0d6ad73a6994315f31aec9
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-05-11 17:31:04 +0300

    refactored

--- lib/hexlet_code/rendered_views/html/tag.rb
+++ lib/hexlet_code/rendered_views/html/tag.rb
@@ -2,18 +2,14 @@
 
 module HexletCode
   module Tag
-    UNPAIRED = %w[area base br col embed hr img input link meta source].freeze
+    UNPAIRED = %i[area base br col embed hr img input link meta source].freeze
 
     def self.build(name, options = {})
       pair_body = yield if block_given?
-      html_form = open_tag_with(name, attributes(options))
-      html_form << "\n" if name == 'form'
+      html_form = "<#{name}#{attributes(options)}>"
+      html_form << "\n" if name == :form
       html_form << pair_body.to_s << "</#{name}>" unless UNPAIRED.include? name
       html_form
-    end
-
-    def self.open_tag_with(name, attrs)
-      "<#{name}#{attrs}>"
     end
 
     def self.attributes(options)
