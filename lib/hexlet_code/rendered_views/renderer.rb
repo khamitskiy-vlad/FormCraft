9b566308c68c2721ef44709e7ac47355e475ef74
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-05-11 18:19:55 +0300

    return to_html method

--- lib/hexlet_code/rendered_views/renderer.rb
+++ lib/hexlet_code/rendered_views/renderer.rb
@@ -4,13 +4,20 @@ module HexletCode
   class Renderer
     include Html
 
-    attr_accessor :form, :form_fields
-    attr_reader :collection
+    attr_accessor :collection
+    attr_reader :form, :fields
 
     def initialize(collection)
       @collection = collection
-      @form_fields = String.new
+      @fields = String.new
       @form = String.new
+    end
+
+    def to_html
+      form_hash = collection[0]
+      inputs_hash = collection[1..]
+      render_html_fields_from(inputs_hash)
+      form.concat(form_for(form_hash[:tag], form_hash[:attributes], fields))
     end
   end
 end
