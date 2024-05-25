6bb1388fb1f2aac037186470ba258c6a70732aa2
khamitskiy-vlad <vladislaviktorovic@gmail.com>
2024-05-25 17:25:12 +0300

    now only action input is supported

--- lib/form_craft/form_tags/form.rb
+++ lib/form_craft/form_tags/form.rb
@@ -6,9 +6,8 @@ module FormCraft
     DEFAULT_METHOD = 'post'
 
     def self.render(form, fields)
-      attributes = { action: form[:attributes][:url] || DEFAULT_ACTION,
-                     method: DEFAULT_METHOD }.merge(form[:attributes]
-                                             .except(:url))
+      attributes = { action: DEFAULT_ACTION,
+                     method: DEFAULT_METHOD }.merge(form[:attributes])
       Tag.build(form[:tag], **attributes) { fields }
     end
   end
